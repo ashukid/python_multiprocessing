30b430f85363857ec0876d64293717f0831c1564
ashukid <irm2015001@iiita.ac.in>
2018-01-20 00:04:50 +0530

    added association rule

--- apriori.py
+++ apriori.py
@@ -1,6 +1,7 @@
 import itertools
+from itertools import chain
 threshold=0.25
-confidence=0.5
+confidence=0.25
 
 file_name="transaction.txt"
 
@@ -16,8 +17,8 @@ total_rows=len(data)
 frequent_set=[]
 support_set=[]
 # class for calculating the frequent set
-def calculate(epoch,last_unique):
-    print(last_unique,epoch)
+def calculate_frequent_set(epoch,last_unique):
+    # print(last_unique,epoch)
     all_candid=list(itertools.combinations(last_unique,epoch)) # candid geenration
 
     support=[]
@@ -57,11 +58,44 @@ for i in range(total_rows):
 current_unique=list(current_unique)
 while True:
 # for i in range(6):
-    unique=calculate(epoch,current_unique)
+    unique=calculate_frequent_set(epoch,current_unique)
     if(unique==0):
         break
     current_unique=unique
     epoch+=1
     
-print("frequent set is : {}, {}".format(frequent_set,support_set))
+print("Frequent set : {} with suuport : {}".format(frequent_set,support_set))
+
+candid_support={}
+for i in range(len(frequent_set)):
+    candid_support[frequent_set[i]]=support_set[i]
+
+
+def calculate_assoc_rule(frequent_set,support_set):
+
+    for i in range(len(frequent_set)):
+        current_element=set(frequent_set[i])
+        all_candid=[]
+        for j in range(1,len(current_element)):
+            all_candid.append(map(set,itertools.combinations(current_element,j)))
+
+        all_candid=list(chain.from_iterable(all_candid))
+
+        for j in range(len(all_candid)):
+            current_candid=all_candid[j]
+            remain_candid=current_element-current_candid
+            total_candid=current_candid.union(remain_candid)
+            total_candid=tuple(total_candid)
+            current_candid=tuple(current_candid)
+            remain_candid=tuple(remain_candid)
+
+            current_confidence=candid_support[total_candid]/float(candid_support[current_candid])
+            if(current_confidence>=confidence):
+                print("Assoc rule {} --> {} with Confidence : {}".format(current_candid,remain_candid,current_confidence))
+
+calculate_assoc_rule(frequent_set,support_set)
+
+
+
+
 
