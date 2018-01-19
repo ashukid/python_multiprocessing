365f69cc4e326a35f1e0d2709f0e44c29fbb5c9e
ashukid <irm2015001@iiita.ac.in>
2018-01-19 20:17:05 +0530

    testing the code

---
+++ example.py
@@ -0,0 +1,24 @@
+#Python 3.5
+import pandas as pd
+from mlxtend.preprocessing import OnehotTransactions
+from mlxtend.frequent_patterns import apriori
+
+# Load CSV
+import pandas
+
+filename = "D:\\Audience_TF_100.csv"
+data = pandas.read_csv(filename)
+
+import pandas as pd
+dataNew = pd.get_dummies(data)
+
+df = pd.DataFrame(dataNew, columns=dataNew.columns)
+
+frequent_itemsets = apriori(df, min_support=0.9, use_colnames=True)
+
+frequent_itemsets['length'] =frequent_itemsets['itemsets'].apply(lambda x: len(x))
+frequent_itemsets
+
+# Create patterns block to retreive latter
+patterns = association_rules(frequent_itemsets, metric="lift", min_threshold=1.)
+patterns
--- test.py
+++ test.py
@@ -4,34 +4,20 @@ import pandas as pd
 from mlxtend.preprocessing import OnehotTransactions
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+
 import time
 
-from multiprocessing import Pool
 
-
+t=time.time()
 filename = "combine.csv"
 data = pd.read_csv(filename)
 dataNew = pd.get_dummies(data)
+print(time.time()-t)
+
 
 t1=time.time()
 df = pd.DataFrame(dataNew, columns=dataNew.columns)
 frequent_itemsets = apriori(df, min_support=0.9, use_colnames=True)
 frequent_itemsets['length'] =frequent_itemsets['itemsets'].apply(lambda x: len(x))
 print(frequent_itemsets)
-print("time taken = {}".format(time.time()-t1))
-
-t2=time.time()
-
-def func(df):
-	frequent_itemsets = apriori(df, min_support=0.9, use_colnames=True)
-	frequent_itemsets['length'] =frequent_itemsets['itemsets'].apply(lambda x: len(x))
-	return frequent_itemsets
-
-
-df = pd.DataFrame(dataNew, columns=dataNew.columns)
-p=Pool(4)
-print(p.map(func,))
-print("parallel time taken = {}".format(time.time()-t2))
-p.close()
-p.join()
-
+print("time taken for apriori = {}".format(time.time()-t1))
