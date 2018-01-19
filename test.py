b47b181209865645fd9c84aa284a578639191bc2
ashukid <irm2015001@iiita.ac.in>
2018-01-19 21:08:26 +0530

    time taken by different section of code

--- test.py
+++ test.py
@@ -12,7 +12,10 @@ t=time.time()
 filename = "combine.csv"
 data = pd.read_csv(filename)
 dataNew = pd.get_dummies(data)
-print(time.time()-t)
+# oht = OnehotTransactions()
+# dataNew = oht.fit(data).transform(data)
+# dataNew=pd.DataFrame(dataNew,columns=oht.columns_)
+print("time taken for input = {}".format(time.time()-t))
 
 
 t1=time.time()
@@ -20,4 +23,4 @@ df = pd.DataFrame(dataNew, columns=dataNew.columns)
 frequent_itemsets = apriori(df, min_support=0.9, use_colnames=True)
 frequent_itemsets['length'] =frequent_itemsets['itemsets'].apply(lambda x: len(x))
 print(frequent_itemsets)
-print("time taken for apriori = {}".format(time.time()-t1))
+print("time taken by apriori = {}".format(time.time()-t1))
